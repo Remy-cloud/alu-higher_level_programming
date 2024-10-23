8514005be44be19b6b6bb143ef0bdf1497ff9763
Remy-cloud <r.intwali@alustudent.com>
2024-10-23 22:02:23 +0200

    import

---
+++ python-import_modules/5-variable_load.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Import the variable a from variable_load_5.py
+from variable_load_5 import a
+
+# Only execute the following when the script is run directly
+if __name__ == "__main__":
+    # Print the value of a
+    print(a)
