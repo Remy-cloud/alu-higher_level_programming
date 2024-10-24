ec033fb3a46a608016f72c74764a94e1ba198e15
Remy-cloud <r.intwali@alustudent.com>
2024-10-24 23:44:38 +0200

    roman to interger

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
+    if type(roman_string) != str or not roman_string:
         return 0
-    """ Roman numeral to integer mapping """
-    roman_numerals = {
-        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+
+    roms = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
     }
 
-    # Initialize the total and previous value
-    total = 0
-    prev_value = 0
+    num = 0
+    for i, x in enumerate(roman_string):
+        num += roms[x]
+        if i != 0 and roms[roman_string[i-1]] < roms[x]:
+            num -= 2 * roms[roman_string[i-1]]
 
-    # Loop through the Roman numeral string from right to left
-    for char in reversed(roman_string):
-        value = roman_numerals.get(char, 0)
-    """" If the current value is less than the previous one,
-     subtract it, otherwise add it"""
-    if value < prev_value:
-        total -= value
-    else:
-        total += value
-
-        # Update the previous value to the current one
-    prev_value = value
-
-    return total
+    return num
