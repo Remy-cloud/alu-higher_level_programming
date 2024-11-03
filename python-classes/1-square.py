455f42dbc5d39c67b8618a85d72fb3f8abb2167b
Remy-cloud <r.intwali@alustudent.com>
2024-11-03 02:09:10 +0200

    class with attribute

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""creating a class"""
 class Square:
+
+    """assigning it with attributes"""
+
     def __init__(self, size):
+
+        """initializing the class"""
+
         self.size = size
+
