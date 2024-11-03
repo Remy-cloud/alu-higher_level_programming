48967f43b4bc94deba8ccb23541244da41b56833
Remy-cloud <r.intwali@alustudent.com>
2024-11-03 03:27:22 +0200

    remove errors

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -11,18 +11,21 @@ class Square:
         self.size = size
         """Retrieves the size of the square"""
 
-        @property
-        def size(self):
-            return self.__size
+    @property
+    def size(self):
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            """raise"""
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        """raise"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
-      return self.__size ** 2
+        """
+        Calculate the area of the square.
+        """
+        return self.__size ** 2
