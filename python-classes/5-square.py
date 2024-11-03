38dd2d49f7f5d7479bb3697745df7c5b3626f939
Remy-cloud <r.intwali@alustudent.com>
2024-11-03 03:32:22 +0200

    print #

---
+++ python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by: (based on 3-square.py)
+
+"""
+
+
+class Square:
+    """Represents a square with a private size attribute,
+     including getter and setter."""
+    def __init__(self, size=0):
+        self.size = size
+        """Retrieves the size of the square"""
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """raise"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Calculate the area of the square.
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        Prints the square using the character '#'.
+        If the size is 0, it prints an empty line.
+        """
+        if self.__size == 0:
+            print("")
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
