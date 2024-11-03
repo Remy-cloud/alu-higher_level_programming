68e20ab863dc2172232571a4846b2512cfa90db4
Remy-cloud <r.intwali@alustudent.com>
2024-11-03 02:02:21 +0200

    empty class

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
+
+
 class Square:
+    """
+    This class represents a square.
+    """
+
     pass
-# Test code
-if __name__ == "__main__":
-    mysquare = Square()
-    print(type(mysquare))
-    print(mysquare.__dict__)
