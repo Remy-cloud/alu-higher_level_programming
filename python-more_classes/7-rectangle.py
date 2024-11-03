b612a804b423a9db95a2ddcb0ecb50a7edb973ef
Remy-cloud <r.intwali@alustudent.com>
2024-11-04 00:15:05 +0200

    print symbol

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -59,8 +59,8 @@ class Rectangle:
         using the '#' character."""
         if self.width == 0 or self.height == 0:
             return ""
-        return "\n".join([str(self.print_symbol)
-                          * self.width for _ in range(self.height)])
+        return "\n".join([str(self.print_symbol) * self.width
+                          for _ in range(self.height)])
 
     def __repr__(self):
         """return a string representation of the rectangle
