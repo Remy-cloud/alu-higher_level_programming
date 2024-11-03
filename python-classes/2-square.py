f423c65cfdba912097f9c42f0cc89179c8b6752d
Remy-cloud <r.intwali@alustudent.com>
2024-11-03 02:36:45 +0200

    private instance attri

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,9 +5,13 @@
 
 
 class Square:
+
+    """give class attributes"""
     def __init__(self, size=0):
+        """raising errors"""
         if not type(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        """initialization"""
         self.__size = size
