9638eb85d4b59385529c7acd3b2f92ce05804997
Remy-cloud <r.intwali@alustudent.com>
2024-11-03 23:29:08 +0200

    area of the rectangle

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""creating a class rectangle """
+
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        """initialize the rectangle instance"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """get width of the rectangle"""
+        return self.width
+
+    @width.setter
+    def width(self, value):
+        """set the value of width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """get height of the rectangle"""
+        return self.height
+
+    @height.setter
+    def height(self, value):
+        """set the height of the rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """calculate the area of the rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
