edf9a0382b84cced0e512807d02fa188198b1bf5
Remy-cloud <r.intwali@alustudent.com>
2024-11-06 23:43:33 +0200

    raise exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""creation of a class"""
+
+
+class BaseGeometry:
+    """function that raises exception """
+    def area(self):
+        raise Exception("area() is not implemented")
