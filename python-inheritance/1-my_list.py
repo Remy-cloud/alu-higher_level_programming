f704536899c06153f0a18036b7e59043a8e35e11
Remy-cloud <r.intwali@alustudent.com>
2024-11-07 13:15:56 +0200

    renamed the class

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,8 +2,11 @@
 """creation of class that inherits from list"""
 
 
-class Myclass(list):
-    """ subclass of list with an additional method to print the list sorted"""
+class MyList(list):
+    """
+    subclass of list with an additional
+    method to print the list sorted
+    """
 
     def print_sorted(self):
         """prints out the sorted list"""
