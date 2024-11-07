6995e317751203a6230bfc092ac65b1cc5eee6e6
Remy-cloud <r.intwali@alustudent.com>
2024-11-07 12:55:47 +0200

    print sorted list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,6 +7,4 @@ class Myclass(list):
     method to print the list sorted"""
 
     def print_sorted(self):
-        copied = self.copy()
-        copied.sort()
-        print(copied)
+        print(self.sort())
