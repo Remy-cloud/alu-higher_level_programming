e51a07fce9d7ff52b84aed2c4e374428d59a7f5c
Remy-cloud <r.intwali@alustudent.com>
2024-11-08 21:31:43 +0200

    new commit

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
-"""creation of a class"""
+"""
+This module defines a BaseGeometry class.
+"""
 
 
 class BaseGeometry:
-    """function that raises exception """
+    """
+    This module defines a BaseGeometry class.
+    """
+
     def area(self):
+        """
+        Raises an exception indicating that the area method is not implemented.
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates that value is an integer greater than 0"""
-        if not isinstance(value, int):
+        """
+        Validates that the provided value is an integer and greater than 0.
+        """
+
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
+
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
