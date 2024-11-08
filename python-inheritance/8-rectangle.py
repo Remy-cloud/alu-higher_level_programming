2d82ac92fc9066bea25de43928ffb579b88103cd
Remy-cloud <r.intwali@alustudent.com>
2024-11-08 10:01:48 +0200

    correct validation errors

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
 """ class Rectangle that inherits from BaseGeometry"""
 
 
-BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
-
 class Rectangle(BaseGeometry):
     """rectangle that inherits from BaseGeometry"""
-    def __int__(self,width,height):
+    def __int__(self, width, height):
         """initialize rectangle instances"""
 
         self.integer_validator("width, width")
         self.__width = width
 
         self.integer_validator("height, height")
-        self.__height =height
+        self.__height = height
