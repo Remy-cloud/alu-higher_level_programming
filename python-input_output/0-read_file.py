7d34338ff489a351736d7081deaeb1cc63e70897
Remy-cloud <r.intwali@alustudent.com>
2024-11-11 21:44:04 +0200

    validation errors

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,4 +2,6 @@
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, encoding="utf-8") as file:
+        """Opens the file using UTF-8 encoding and ensures 
+        the file is properly closed after the block is executed."""
         print(file.read(), end="")
