6b2c1d64e6c802a36103b9ddfdcaf7123cabfbe0
Remy-cloud <r.intwali@alustudent.com>
2024-11-11 23:35:36 +0200

    attrs

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""class creation"""
+
+
+class Student:
+    """defines a student by first name, last name, and age."""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only those attributes will be included.
+        Otherwise, all attributes will be included.
+        """
+        if isinstance(attrs, list) and all(isinstance(attr, str) for
+                                           attr in attrs):
+            return {key: getattr(self, key) for
+                    key in attrs if hasattr(self, key)}
+        return self.__dict__
