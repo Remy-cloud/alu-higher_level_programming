eff2d087b4befdca7336eea0b65b6fd86e285d5d
Remy-cloud <r.intwali@alustudent.com>
2024-11-11 23:16:42 +0200

    documeting the file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""module that has a script that adds all arguments to a Python list,
+"""a module that has a script that adds all arguments to a Python list,
  and then save them to a file:"""
 
 
 import sys
-
-
 from 5-save_to_json_file.py import save_to_json_file
 from 6-load_from_json_file.py import load_from_json_file
 
