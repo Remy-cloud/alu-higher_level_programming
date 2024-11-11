d0beba232c52e10c4c28b5d65adeef297ace01a2
Remy-cloud <r.intwali@alustudent.com>
2024-11-11 23:48:13 +0200

    document file

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def pascal_triangle(n):
     """
-    Returns a list of lists representing the Pascal's triangle of height n.
-
-    Args:
-        n (int): The number of rows in the Pascal's triangle.
-
-    Returns:
-        List[List[int]]: Pascal's triangle as a list of lists of integers.
+    Returns a list of lists representing the Pascal's triangle of height n
     """
     # If n is less than or equal to 0, return an empty list
     if n <= 0:
@@ -15,10 +9,10 @@ def pascal_triangle(n):
 
     triangle = []
 
-    # Generate each row of the Pascal's triangle
+    """Generate each row of the Pascal's triangle"""
     for i in range(n):
-        row = [None] * (i + 1)  # Create a row with None placeholders
-        row[0], row[-1] = 1, 1  # First and last elements are always 1
+        row = [None] * (i + 1)
+        row[0], row[-1] = 1, 1
 
         # Fill in the interior elements
         for j in range(1, len(row) - 1):
