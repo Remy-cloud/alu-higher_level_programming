44425d93394f0dade8c14ffa81a74cdea8a4899b
Remy-cloud <r.intwali@alustudent.com>
2024-11-11 23:09:39 +0200

    add all agrument to python list

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""module that has a script that adds all arguments to a Python list,
+ and then save them to a file:"""
+
+
+import sys
+
+
+from 5-save_to_json_file.py import save_to_json_file
+from 6-load_from_json_file.py import load_from_json_file
+
+
+filename = "add_item.json"
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
+
+items.extend(sys.argv[1:])
+
+save_to_json_file(items, filename)
