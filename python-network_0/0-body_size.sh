f16f099beaba389b134699fa9303785ffe807fce
Remy-cloud <r.intwali@alustudent.com>
2024-11-22 22:58:42 +0000

    updates

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Uses curl on the first argument and prints the size of it.
-curl -s "$1" | wc -c
+# Uses curl on the first argument and prints the size of the body of the response.
+curl -s -w '%{size_download}\n' -o /dev/null "$1"
+
