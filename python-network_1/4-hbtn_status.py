afc3a5c191abdb2fbdcca429dc4278e171224643
Remy-cloud <r.intwali@alustudent.com>
2024-11-23 15:52:30 +0000

    new update

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""
-    This script makes a request and displays a
-    response using the requests package.
-"""
-
+#This script makes a request and displays a response using the requests package.
 import requests
 
 if __name__ == "__main__":
-    response = requests.get("https://alu-intranet.hbtn.io/status")
-
+    # URL to fetch the response from
+    url = 'https://alu-intranet.hbtn.io/status'
+    
+    # Send a GET request to the URL
+    response = requests.get(url)
+    
+    # Display the body of the response with the required tabulation
     print("Body response:")
-    print(f"\t- type: {type(response.text)}")
-    print(f"\t- content: {response.text}")
+    print("\t- {}".format(response.text))
