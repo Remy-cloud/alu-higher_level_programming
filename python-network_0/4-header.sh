533f7ee1b7d4e56bf1bd82377b74e0907131a9ea
Remy-cloud <r.intwali@alustudent.com>
2024-11-23 13:41:14 +0000

    sends a GET request to the URL

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends GET request with custom header X-HolbertonSchool-User-Id: 98
-curl -sH "X-HolbertonSchool-User-Id: 98" "http://0.0.0.0:5000$1"
+curl -s -H "X-HolbertonSchool-User-Id: 98" "http://127.0.0.1:5000$1"
