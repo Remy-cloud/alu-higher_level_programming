e82cc63fd2b70adb518c7e8ed21328fb09d132e0
Remy-cloud <r.intwali@alustudent.com>
2024-11-23 13:54:12 +0000

    new commit

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends request with an added custom header
+# This script sends a GET request to the URL passed and  displays the body of the response.
 curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
