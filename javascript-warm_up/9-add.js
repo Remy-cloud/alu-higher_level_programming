8eb08f1a1be3737d858b7e5b57bee297fb4cacf1
Remy-cloud <r.intwali@alustudent.com>
2025-01-26 16:08:41 +0200

    additiion

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 const args1 = parseInt(process.argv[2]);
 const args2 = parseInt(process.argv[3]);
 
-function add(a, b){
-    return a + b;
+function add (a, b) {
+  return a + b;
 }
 console.log(add(args1, args2));
