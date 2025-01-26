cb27222adaaf0494ab7263d118f50a50f9d4e41b
Remy-cloud <r.intwali@alustudent.com>
2025-01-26 14:01:08 +0200

    print three lines

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let firstline = 'C is fun';
-let secondline = 'python is cool';
+let secondline = 'Python is cool';
 let thirdline = 'JavaScript is amazing';
 console.log(firstline);
 console.log(secondline);
