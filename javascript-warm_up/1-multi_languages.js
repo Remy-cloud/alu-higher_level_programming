63217453172ebe6ed7a79e9b05f702d3b3bbe4af
Remy-cloud <r.intwali@alustudent.com>
2025-01-26 14:02:41 +0200

    semistandard validation correction

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let firstline = 'C is fun';
-let secondline = 'Python is cool';
-let thirdline = 'JavaScript is amazing';
+const firstline = 'C is fun';
+const secondline = 'Python is cool';
+const thirdline = 'JavaScript is amazing';
 console.log(firstline);
 console.log(secondline);
 console.log(thirdline);
