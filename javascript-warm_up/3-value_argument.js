d0fc70e4a8bca83eef30c546713229388a559b10
Remy-cloud <r.intwali@alustudent.com>
2025-01-26 14:59:04 +0200

    syntax errors correction

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-if (args[0] === undefined) {
-  console.log('No agrument');
+if (Process.argv[2]) {
+  console.log('process.argv[2]');
 } else {
-  console.log('args[0]');
+  console.log('No argument');
 }
