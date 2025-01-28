d7129e8a1940dbf40f7c3a419275c7334c41eade
Remy-cloud <r.intwali@alustudent.com>
2025-01-28 22:29:01 +0200

    capitalization of square

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const Rectangle = require('./4-rectangle');
 
-class square extends Rectangle {
+class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
 }
-module.exports = square;
+module.exports = Square;
