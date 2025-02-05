c9cbbc62f69a6b9395e25b813e491edb2e2c14cf
Remy-cloud <r.intwali@alustudent.com>
2025-02-05 21:01:38 +0200

    a script that computes the number of tasks completed by user id.

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  try {
+    const todos = JSON.parse(body);
+    const completedTasks = {};
+
+    todos.forEach((task) => {
+      if (task.completed) {
+        if (!completedTasks[task.userId]) {
+          completedTasks[task.userId] = 0;
+        }
+        completedTasks[task.userId]++;
+      }
+    });
+
+    console.log(completedTasks);
+  } catch (err) {
+    console.error(err);
+  }
+});
